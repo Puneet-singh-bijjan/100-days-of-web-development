42c1943b19cb5b0d0822ec1b90aff82567b6c9b7
Puneet Singh <puneetsingh8120@gmail.com>
2022-01-25 17:18:02 +0530

    learning javaScript

---
+++ learning_JavaScript/app.js
@@ -0,0 +1,11 @@
+let name = "Puneet";
+let age = 21;
+let hobbies = ['Coding' , 'Cricket' , 'Listening Music'];
+let languages = {
+    english : 'yes',
+    rating : 9,
+    proficiency : 'expert'
+};
+
+alert(hobbies);
+alert(languages.proficiency);
