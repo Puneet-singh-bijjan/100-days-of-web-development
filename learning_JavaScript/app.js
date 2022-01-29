bee4ac6c2837e643efbdeddcc9e6954d615c2afc
Puneet Singh <puneetsingh8120@gmail.com>
2022-01-29 22:46:21 +0530

    .

--- learning_JavaScript/app.js
+++ learning_JavaScript/app.js
@@ -9,3 +9,27 @@ parentElement.appendChild(newElement);
 console.dir(document.body);
 
 newElement.remove();
+
+let toMove = document.querySelector('p');
+document.body.append(toMove);
+
+toMove.remove();
+document.querySelector('p').remove();
+
+let count = 0;
+
+function countValve (event){
+    console.log(event.target.value);
+    console.log(++count);
+}
+
+document.querySelector('input').addEventListener('input' , countValve);
+
+newElement = document.createElement('p');
+newElement.textContent = 'click me';
+document.body.append(newElement);
+
+function clickMeListener(){
+    newElement.textContent = 'Clicked';
+}
+newElement.addEventListener('click' , clickMeListener);
