9eff9f00b7e52d4211041fa468e35b8cece2d07f
Puneet Singh <puneetsingh8120@gmail.com>
2022-01-29 22:27:46 +0530

    .

--- learning_JavaScript/app.js
+++ learning_JavaScript/app.js
@@ -1,38 +1,11 @@
-let name = "Puneet";
-let age = 21;
-let hobbies = ['Coding' , 'Cricket' , 'Listening Music'];
-let languages = {
-    english : 'yes',
-    rating : 9,
-    proficiency : 'expert'
-};
+let newElement = document.createElement('a');
+newElement.href = 'https://instagram.com';
+newElement.textContent = 'Instagram';
 
-let onlineCourse = {
-    name:'Web Development by maximilian',
-    price: 400,
-    target: ['learning' , 'practicing' , 'what to do next'], 
-    calling(array , arrayItem){
-        return array[arrayItem];
-    }
-     
-};
+let parentElement = document.querySelector('h1');
 
-console.log(onlineCourse.calling(onlineCourse.target , 0));
+parentElement.appendChild(newElement);  
 
-function calling(array , arrayItem){
-    return array[arrayItem];
-};
+console.dir(document.body);
 
-let i=0,j=0;
-for(i=0;i<=4;i++){
-    for(j=0;j<=i;j++){
-        console.log('*');
-    }console.log('\n');
-}
-
-let x = 21;
-if(x%2==0){
-    console.log('Prime Number...')
-}else{
-    console.log('Not a Prime Number...')
-}
+newElement.remove();
