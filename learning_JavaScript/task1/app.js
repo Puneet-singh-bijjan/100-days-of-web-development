579550b4cc2d367c88129248defd1e5c65f64f42
Puneet Singh <puneetsingh8120@gmail.com>
2022-01-30 18:42:06 +0530

    added if else

--- learning_JavaScript/task1/app.js
+++ learning_JavaScript/task1/app.js
@@ -1,7 +1,14 @@
+let remainingCharacter;
 function countInput(event){
-    let x = 60 - event.target.value.length;
-    document.getElementById('toChangeSpan').textContent = x;
-   
+    remainingCharacter = 60 - event.target.value.length;
+     let toChangeSpan = document.getElementById('toChangeSpan');
+     toChangeSpan.textContent = remainingCharacter;
+   if(remainingCharacter<10){
+       document.querySelector('input').style.backgroundColor = 'rgb(223, 91, 58)';
+       toChangeSpan.style.color =  'rgb(223, 91, 58)';
+   }else{
+    document.querySelector('input').style.backgroundColor = 'white';   
+   }
 }
 
 document.getElementById('text-input').addEventListener('input' , countInput);
