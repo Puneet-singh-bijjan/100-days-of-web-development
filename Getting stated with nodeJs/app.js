936d560eee7241a2cdfcb00e5f689425ee935843
Puneet Singh <puneetsingh8120@gmail.com>
2022-02-10 16:57:17 +0530

    ...

---
+++ Getting stated with nodeJs/app.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+function handleRequest(request, response) {
+
+    if (request.url == '/currenttime') {
+        response.statusCode = 200;
+        response.end(Date());
+
+    } else {
+        response.statusCode = 200;
+        response.end('<h1>Hello World!</h1>');
+    }
+
+
+}
+
+const server = http.createServer(handleRequest);
+
+server.listen(3000);
