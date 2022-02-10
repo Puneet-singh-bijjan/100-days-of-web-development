947f754f88dda2f16ed03204246d6a439d04dab6
Puneet Singh <puneetsingh8120@gmail.com>
2022-02-10 18:55:49 +0530

    getting started with express and nom

--- Getting stated with nodeJs/app.js
+++ Getting stated with nodeJs/app.js
@@ -1,19 +1,47 @@
-const http = require('http');
 
-function handleRequest(request, response) {
+const { urlencoded } = require('body-parser');
+const express = require('express');
 
-    if (request.url == '/currenttime') {
-        response.statusCode = 200;
-        response.end(Date());
+const app = express();
+app.use(express.urlencoded({extended : false}));
 
-    } else {
-        response.statusCode = 200;
-        response.end('<h1>Hello World!</h1>');
-    }
+app.get('/' , function(request , response){
+    const toSendForm = '<form method="post" action="/store-data"><label>Your Name:</label><input type="text" name="username"/><button>Submit</button></form>'
+    response.send(toSendForm);
+});
+
+app.get('/currenttime' , function(request , response) { // or req , res
+    const stringData = '<h1>' + new Date().toISOString() + '</h1>';
+    response.send(stringData);
+});
+
+app.post('/store-data' , function(request , response){
+    const userData = request.body.username;
+    console.log(userData);
+    response.send('<h1>Data submitted Successfully!');
+})
+
+app.listen(3000);
 
 
-}
 
-const server = http.createServer(handleRequest);
 
-server.listen(3000);
+// const http = require('http');
+
+// function handleRequest(request, response) {
+
+//     if (request.url == '/currenttime') {
+//         response.statusCode = 200;
+//         response.end(Date());
+
+//     } else {
+//         response.statusCode = 200;
+//         response.end('<h1>Hello World!</h1>');
+//     }
+
+
+// }
+
+// const server = http.createServer(handleRequest);
+
+// server.listen(3000);
